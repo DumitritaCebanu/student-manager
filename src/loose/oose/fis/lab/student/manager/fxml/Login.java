33c3d9c13ef66d60f12fb7398d7a5260e6eb9f98
Cebanu Dumitrita <dumitrita.cebanu.99@gmail.com>
2020-03-24 20:50:53 +0200

    SM-1 Added the login screen

---
+++ src/loose/oose/fis/lab/student/manager/fxml/Login.java
@@ -0,0 +1,8 @@
+package loose.oose.fis.lab.student.manager.fxml;
+
+import javafx.event.ActionEvent;
+
+public class Login {
+    public void handleLoginButtonAction(ActionEvent actionEvent) {
+    }
+}
